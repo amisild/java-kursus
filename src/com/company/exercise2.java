a86b66ff2a93d35848407d9253a0c596c97a1040
amisild <56818531+amisild@users.noreply.github.com>
2020-04-16 15:32:30 +0300

    Update exercise2.java

--- src/com/company/exercise2.java
+++ src/com/company/exercise2.java
@@ -1,15 +1,30 @@
 //ülesanne 2 - palgatõus
 package com.company;
 
+import java.util.Scanner;
+
 public class exercise2 {
 
     public static void main(String[] args) {
+        //palgatõusu arvutamine massiivi puhul
         int[] palgad = {947, 1963, 2000, 850, 650, 1000, 708};
 
         for (int i = 0; i < palgad.length; i++) {
             if (palgad[i] < 1000) {
                 System.out.println("Sinu uueks palgaks on " + palgad[i] * 1.05 + " eurot!");
             }
+        }
+
+        //palgatõusu arvutamine user inputi puhul
+        Scanner scan = new Scanner (System.in);
+        System.out.println("Mis on su palk?");
+        int palk = scan.nextInt();
+
+        if (palk < 1000) {
+            double raise = palk * 1.05;
+            System.out.println("Sinu uueks palgaks on " + raise + " eurot!");
+        } else {
+            System.out.println("Sina palgatõusu ei saa!");
         }
     }
 }
