e4976cf21cb822f8761907597f175931f2cedd55
amisild <56818531+amisild@users.noreply.github.com>
2020-05-23 09:01:02 +0300

    Update and rename homework03.java to Homework03.java

--- src/kolmashomework/Homework03.java
+++ src/kolmashomework/Homework03.java
@@ -7,7 +7,7 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.Scanner;
 
-public class homework03 {
+public class Homework03 {
 
     public static void main(String[] args) {
         List<List<String>> fail = filereader();
